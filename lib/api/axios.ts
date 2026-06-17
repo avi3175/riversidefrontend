421b99c9d7abc69bfb628f0d8cb7117beb344fef
avi3175 <avishahriar@.com>
2026-06-17 12:49:39 +0600

    project is deployed but Dynamic not working

--- lib/api/axios.ts
+++ lib/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL
-const API_BASE_URL = 'https://riverviewbackend.vercel.app/api/v1'
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL
+// const API_BASE_URL = 'https://riverviewbackend.vercel.app/api/v1'
 export const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
